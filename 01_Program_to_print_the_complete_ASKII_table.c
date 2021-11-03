3accce539ca3cf58f7dfb13a2307340d38569702
Gautami Avinash Shetty <92733988+ShettyGA@users.noreply.github.com>
2021-11-03 20:37:04 +0530

    Add files via upload

---
+++ 01_Program_to_print_the_complete_ASKII_table.c
@@ -0,0 +1,18 @@
+// In 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int askii = 0;
+    printf("\n\t Your ASKII table is below => \n");
+
+    while(askii<128)
+        {
+            printf("\t %3d = %c. \n",askii, askii);
+            ++askii;
+        }
+    printf("\n\tThanks for visiting");    
+
+    getch();
+    return 0;
+}
---
+++ 02_Program_to_get_the_ASKII_value_of_the_given_character.c
@@ -0,0 +1,15 @@
+//This code is to find out ASKII value of given character 
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num_1 = 0;
+    printf("Enter the character to find its ASKII value: \n");
+    scanf("%c",&num_1);
+
+    printf("Your ASKII value for the given character is %d",num_1);
+
+    getch();
+    return 0;
+}
