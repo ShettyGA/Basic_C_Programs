5054f732d3523b33080547ee897f86c0758ff783
Gautami Avinash Shetty <92733988+ShettyGA@users.noreply.github.com>
2021-11-05 20:29:41 +0530

    Update 12_B_Program_to_perform_basic_arithmetic_operation.c

--- 12_B_Program_to_perform_basic_arithmetic_operation.c
+++ 12_B_Program_to_perform_basic_arithmetic_operation.c
@@ -4,13 +4,13 @@ int main()
 {
     int num1 = 0, num2 = 0;
     char oprtr = '0'; 
-
-    printf("\nChoose the operator(+,-,*,/,%%): ");
-    scanf("%c",&oprtr);
-
+    
     printf("\nEnter two number: ");
     scanf("%d%d",&num1,&num2);
     
+    printf("\nChoose the operator(+,-,*,/,%%): ");
+    scanf("%c%c",&oprtr,&oprtr);
+
     switch(oprtr)
     {
     case '+':
