b0ac718406575ee5d479e49c3dda984d6fd66d1f
Gautami Avinash Shetty <92733988+ShettyGA@users.noreply.github.com>
2021-11-05 20:51:09 +0530

    Update 09_C_Program_to_print_multipication_table_in_a_given_range.c

--- 09_C_Program_to_print_multipication_table_in_a_given_range.c
+++ 09_C_Program_to_print_multipication_table_in_a_given_range.c
@@ -3,19 +3,36 @@
 int main()
 {
     int row = 0, column = 0, s_num = 0, e_num = 0;
+    
     printf("Enter the range's starting number: \n");
     scanf("%d",&s_num);
+    
     printf("Enter the range's ending number: \n");
     scanf("%d",&e_num);
-
-    for(row = 1; row <= 10; row++)
+    
+    if(s_num< e_num)
     {
-        for(column = s_num; column<= e_num; column++)
+        for(row = 1; row <= 10; row++)
         {
-            printf("%2d ",row*column);
+            for(column = s_num; column<= e_num; column++)
+            {
+                printf("%2d ",row*column);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
+    else if(e_num<s_num)
+    {
+      for(row = 1; row <= 10; row++)
+        {
+            for(column = s_num; column>= e_num; column--)
+            {
+                printf("%2d ",row*column);
+            }
+            printf("\n");
+        }  
+    }
+    
 
     getch();
     return 0;
